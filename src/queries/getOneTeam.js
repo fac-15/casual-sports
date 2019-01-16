b7b0760d3431f6083bf09475b4b8ba1c57252349
Michal Weizman <m.weizman@gmail.com>
2019-01-16 12:35:16 +0000

    Added tests for getOneTeam function

--- src/queries/getOneTeam.js
+++ src/queries/getOneTeam.js
@@ -1,13 +1,11 @@
-const dbConnection = require('../db/db_connection.js');
+const dbConnection = require("../db/db_connection.js");
 
-
-const getOneTeam = id => {
-    return new Promise((resolve, reject) => {
-      dbConnection.query(`SELECT * FROM teams WHERE id = '${id}'`, (err, res) => {
-        if (err) reject(err);
-        else resolve(res.rows);
-      });
+const getOneTeam = id =>
+  new Promise((resolve, reject) => {
+    dbConnection.query(`SELECT * FROM teams WHERE id = '${id}'`, (err, res) => {
+      if (err) reject(`${err}`);
+      else resolve(res.rows[0]);
     });
-  }
+  });
 
-  module.exports = getOneTeam;
+module.exports = getOneTeam;
