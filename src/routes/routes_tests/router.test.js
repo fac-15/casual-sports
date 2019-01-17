d2869d52a3af6b45ac836523bfd8afe9036b4b9a
Michal Weizman <m.weizman@gmail.com>
2019-01-17 17:34:21 +0000

    removed supertest test

--- src/routes/routes_tests/router.test.js
+++ src/routes/routes_tests/router.test.js
@@ -1,17 +1,17 @@
-const supertest = require("supertest");
-const app = require("../../app.js");
+// const supertest = require("supertest");
+// const app = require("../../app.js");
 
-test("SERVER TEST TIME", () => {
-  expect(1).toBe(1);
-});
+// test("SERVER TEST TIME", () => {
+//   expect(1).toBe(1);
+// });
 
-describe("Home route", () => {
-  test("GET to home should return status 200", done => {
-    supertest(app)
-      .get("/")
-      .then(response => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-  });
-});
+// describe("Home route", () => {
+//   test("GET to home should return status 200", done => {
+//     supertest(app)
+//       .get("/")
+//       .then(response => {
+//         expect(response.statusCode).toBe(200);
+//         done();
+//       });
+//   });
+// });
