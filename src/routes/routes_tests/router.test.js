df82847ec8b94996e041b02a7cd22a2250f7c876
Michal Weizman <m.weizman@gmail.com>
2019-01-17 17:21:32 +0000

    second attempt to fix travis

--- src/routes/routes_tests/router.test.js
+++ src/routes/routes_tests/router.test.js
@@ -5,9 +5,13 @@ test("SERVER TEST TIME", () => {
   expect(1).toBe(1);
 });
 
-test("GET to home should return status 200", () =>
-  supertest(app)
-    .get("/")
-    .then(res => {
-      expect(res.statusCode).toBe(200);
-    }));
+describe("Home route", () => {
+  test("GET to home should return status 200", done => {
+    supertest(app)
+      .get("/")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
