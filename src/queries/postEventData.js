1fba075bf8ff976fa0d949e699f30d18e9934f51
Michal Weizman <m.weizman@gmail.com>
2019-01-18 11:46:19 +0000

    Added date time fields

--- src/queries/postEventData.js
+++ src/queries/postEventData.js
@@ -2,10 +2,12 @@ const dbConnection = require("../db/db_connection.js");
 
 const postEvent = newEvent => {
   return new Promise((resolve, reject) => {
-    const query = `INSERT INTO events (name, sport, users_id, location, genders, open) VALUES ($1, $2, $3, $4, $5, $6)`;
+    const query = `INSERT INTO events (name, sport, date, time users_id, location, genders, open) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
     const values = [
       newEvent.name,
       newEvent.sport,
+      newEvent.date,
+      newEvent.time,
       "5",
       newEvent.location,
       newEvent.gender,
