9669c60a1899ad2cb9f34b1cd38e3ac9876062f3
Michal Weizman <m.weizman@gmail.com>
2019-01-22 10:56:50 +0000

    added better tests names and described tests

--- src/queries/_tests_/getAllData.test.js
+++ src/queries/_tests_/getAllData.test.js
@@ -2,34 +2,36 @@ const getAllData = require("../getAllData");
 const dbBuild = require("../../db/db_build.js");
 const { events, teams } = require("./test_fixtures");
 
-test("the function returns all of the event data", () => {
-  dbBuild((error, response) => {
-    if (error) return console.log("ERROR IN DBBUILD: " + error);
-    expect.assertions(1);
-    return expect(getAllData.getTableData("events")).resolves.toEqual(events);
+describe("Check that getTableData returns the appropriate data/errors", () => {
+  test("getTableData returns all of the events table data", () => {
+    dbBuild((error, response) => {
+      if (error) return console.log("ERROR IN DBBUILD: " + error);
+      expect.assertions(1);
+      return expect(getAllData.getTableData("events")).resolves.toEqual(events);
+    });
+  });
+
+  test("getTableData fails with an error", () => {
+    dbBuild((error, response) => {
+      if (error) return console.log("ERROR IN DBBUILD: " + error);
+      const msg = "OMG EXPLOSIONS";
+      return expect(getAllData.getTableData(new Error())).rejects.toThrow(msg);
+    });
+  });
+
+  test("getTableData returns all of the teams data", () => {
+    dbBuild((error, response) => {
+      if (error) return console.log("ERROR IN DBBUILD: " + error);
+      expect.assertions(1);
+      return expect(getAllData.getTableData("teams")).resolves.toEqual(teams);
+    });
+  });
+
+  test("getTableData fails with an error", () => {
+    dbBuild((error, response) => {
+      if (error) return console.log("ERROR IN DBBUILD: " + error);
+      const msg = "OMG EXPLOSIONS";
+      return expect(getAllData.getTableData(new Error())).rejects.toThrow(msg);
+    });
   });
 });
-
-// test("the function fails with an error", () => {
-//   dbBuild(function(error, response) {
-//     if (error) return console.log("ERROR IN DBBUILD: " + error);
-//     const msg = "OMG EXPLOSIONS";
-//     return expect(getAllData.getTableData(new Error())).rejects.toThrow(msg);
-//   });
-// });
-
-test("the function returns all of the team data", () => {
-  dbBuild((error, response) => {
-    if (error) return console.log("ERROR IN DBBUILD: " + error);
-    expect.assertions(1);
-    return expect(getAllData.getTableData("teams")).resolves.toEqual(teams);
-  });
-});
-
-// test("the function fails with an error", () => {
-//   dbBuild(function(error, response) {
-//     if (error) return console.log("ERROR IN DBBUILD: " + error);
-//     const msg = "OMG EXPLOSIONS";
-//     return expect(getAllData.getTableData(new Error())).rejects.toThrow(msg);
-//   });
-// });
--- src/queries/_tests_/getOneEvent.test.js
+++ src/queries/_tests_/getOneEvent.test.js
@@ -2,20 +2,24 @@ const getOneEvent = require("../getOneEvent");
 const dbBuild = require("../../db/db_build.js");
 const { event } = require("./test_fixtures");
 
-// const error = "error: invalid input syntax for integer: \"potatoes\"";
+const error = {
+  error: `[error: invalid input syntax for integer: "potatoes"]`
+};
 
-test("the function returns the data in the event with the id of 1", () => {
-  dbBuild((error, response) => {
-    if (error) return console.log("ERROR IN DBBUILD: " + error);
-    expect.assertions(1);
-    return expect(getOneEvent(1)).resolves.toEqual(event);
+describe("Check that getOneEvent returns the appropriate data/errors", () => {
+  test("getOneEvent returns the data in the event with the id of 1", () => {
+    dbBuild((error, response) => {
+      if (error) return console.log("ERROR IN DBBUILD: " + error);
+      expect.assertions(1);
+      return expect(getOneEvent(1)).resolves.toEqual(event);
+    });
+  });
+
+  test("getOneEvent fails with invalid input", () => {
+    dbBuild((error, response) => {
+      if (error) return console.log("ERROR IN DBBUILD: " + error);
+      expect.assertions(1);
+      return expect(getOneEvent("potatoes")).rejects.toMatch(error);
+    });
   });
 });
-
-// test("the fetch fails with an error", () => {
-//   dbBuild(function(error, response) {
-//     if (error) return console.log("ERROR IN DBBUILD: " + error);
-//     expect.assertions(1);
-//     return expect(getOneEvent("potatoes")).rejects.toMatch(error);
-//   });
-// });
