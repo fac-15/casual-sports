8ff437452aee80c0f6907e82cc359cc960b721ee
zurda <m.weizman@gmail.com>
2019-01-23 16:20:25 +0000

    improvements to meetup page

--- src/queries/getMeeupApi.js
+++ src/queries/getMeeupApi.js
@@ -7,7 +7,7 @@ const MEETUP_KEY = process.env.MEETUP_KEY;
 if (!MEETUP_KEY) {
   throw new Error("MEETUP_KEY must be set");
 }
-const meetup_url = `https://api.meetup.com/2/open_events?zip=EC1A+4DD&and_text=False&country=gb&offset=0&city=London&format=json&limited_events=False&photo-host=public&page=20&radius=25.0&category=32&status=upcoming&desc=False&sig_id=196338373&sig=${MEETUP_KEY}&key=${MEETUP_KEY}`;
+const meetup_url = `https://api.meetup.com/2/open_events?zip=EC1A+4DD&and_text=False&country=gb&offset=0&city=London&format=json&limited_events=False&photo-host=public&page=20&radius=25.0&category=32&status=upcoming&desc=False&fields=group_photo&sig_id=196338373&sig=${MEETUP_KEY}&key=${MEETUP_KEY}`;
 
 async function getMeetupApi() {
   try {
@@ -17,7 +17,5 @@ async function getMeetupApi() {
     console.error(error);
   }
 }
-
-// getMeetupApi(meetup_url);
 
 module.exports = getMeetupApi;
