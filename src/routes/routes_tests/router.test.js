1cfce01203969097213a1dd64e8547f12d054e2c
saksiva <sakatheesan@gmail.com>
2019-01-23 12:35:26 +0000

    test1

--- src/routes/routes_tests/router.test.js
+++ src/routes/routes_tests/router.test.js
@@ -32,12 +32,12 @@ describe("Check that router GET calls return the appropriate status codes", () =
   //     .then(res => {
   //       expect(res.statusCode).toBe(200);
   //     }));
-  // test("GET to /search/events/chess should return status 200", () =>
-  //   supertest(app)
-  //     .get("/search/events/chess")
-  //     .then(res => {
-  //       expect(res.statusCode).toBe(200);
-  //     }));
+  test("GET to /search/events/chess should return status 200", () =>
+    supertest(app)
+      .get("/search/events/chess")
+      .then(res => {
+        expect(res.statusCode).toBe(200);
+      }));
   // test("GET to /search-open/events/rugby should return status 200", () =>
   //   supertest(app)
   //     .get("/search-open/events/rugby")
