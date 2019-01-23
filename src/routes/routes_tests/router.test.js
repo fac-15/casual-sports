ab34f3ed9419251f904ce83c45aa71cc996d18ed
saksiva <sakatheesan@gmail.com>
2019-01-23 14:39:48 +0000

    test4

--- src/routes/routes_tests/router.test.js
+++ src/routes/routes_tests/router.test.js
@@ -20,11 +20,12 @@ describe("Check that router GET calls return the appropriate status codes", () =
         done();
       });
   });
-  test("GET to anaconda should return status 404", () =>
+  test("GET to anaconda should return status 404", done =>
     supertest(app)
       .get("/anaconda")
       .then(res => {
         expect(res.statusCode).toBe(404);
+        done();
       }));
   // test("GET to /search/teams/football should return status 200", () =>
   //   supertest(app)
@@ -44,18 +45,20 @@ describe("Check that router GET calls return the appropriate status codes", () =
   //     .then(res => {
   //       expect(res.statusCode).toBe(302);
   //     }));
-  test("GET to /add-event should return status 200", () =>
+  test("GET to /add-event should return status 200", done =>
     supertest(app)
       .get("/add-event")
       .then(res => {
         expect(res.statusCode).toBe(200);
+        done();
       }));
-  // test("GET to /add-team should return status 200", () =>
-  //   supertest(app)
-  //     .get("/add-team")
-  //     .then(res => {
-  //       expect(res.statusCode).toBe(200);
-  //     }));
+  test("GET to /add-team should return status 200", done =>
+    supertest(app)
+      .get("/add-team")
+      .then(res => {
+        expect(res.statusCode).toBe(200);
+        done();
+      }));
 });
 
 // describe("Check that making a GET request to a specific team id returns the correct team", () => {
