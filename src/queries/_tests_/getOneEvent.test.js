34057825b48cd9b923f0d7e4c471fe11908861e4
zurda <m.weizman@gmail.com>
2019-01-24 17:35:31 +0000

    fix test description

--- src/queries/_tests_/getOneEvent.test.js
+++ src/queries/_tests_/getOneEvent.test.js
@@ -2,8 +2,8 @@ const getOneEvent = require("../getOneEvent");
 const { refresh } = require("../../db/build.js");
 const { event, error } = require("./test_fixtures");
 
-describe("getOneEvent returns the data in the event with the id of 1", () => {
-  test(`Assert that you can click the profile tab`, async done => {
+describe("testing getOneEvent", () => {
+  test(`getOneEvent returns the data in the event with the id of 1`, async done => {
     await refresh();
     expect(getOneEvent(1)).resolves.toEqual(event);
     done();
--- src/queries/_tests_/getOneUser.test.js
+++ src/queries/_tests_/getOneUser.test.js
@@ -2,8 +2,8 @@ const getOneUser = require("../getOneUser");
 const { refresh } = require("../../db/build.js");
 const { user } = require("./test_fixtures");
 
-describe("getOneUser returns the data in the event with the id of 1", () => {
-  test(`Assert that you can click the profile tab`, async done => {
+describe("getOneUser returns the data in the event with the username charlie", () => {
+  test(`getOneUser returns the data in the event with the username charlie`, async done => {
     await refresh();
     expect(getOneUser("charlie")).resolves.toEqual(user);
     done();
