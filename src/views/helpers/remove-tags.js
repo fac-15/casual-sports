ed224b37d7f2c09bf25e775d46c208cba77465e4
Sandra <grahlsan@gmail.com>
2019-01-24 09:57:00 +0000

    more contrast

--- src/views/helpers/remove-tags.js
+++ src/views/helpers/remove-tags.js
@@ -1,8 +1,10 @@
 const removeTags = str => {
+  if (str === undefined) {
+    return;
+  } else if (str.length > 140) {
   const cleanString = str.replace(/<\/?[^>]+(>|$)/g, "");
-  if (cleanString.length > 140) {
     return cleanString.substring(0, 90) + "...";
-  } return cleanString;
+    }
 };
 
 module.exports = removeTags;
