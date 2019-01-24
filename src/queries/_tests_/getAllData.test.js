1fdd8899f1c6390a40029f5ef33dc0eb66d3e87a
saksiva <sakatheesan@gmail.com>
2019-01-24 22:00:57 +0000

    catch err

--- src/queries/_tests_/getAllData.test.js
+++ src/queries/_tests_/getAllData.test.js
@@ -2,18 +2,28 @@ const getAllData = require("../getAllData");
 const { refresh } = require("../../db/build.js");
 const { events, teams } = require("./test_fixtures");
 
-describe("testing getTableData", () => {
-  test(`getTableData returns all events`, async done => {
+// describe("testing getTableData", () => {
+//   test(`getTableData returns all events`, async done => {
+//     await refresh();
+//     expect(getAllData.getTableData("events")).resolves.toEqual(events);
+//     done();
+//   }, 30000);
+// });
+//
+// describe("getTableData returns all teams", () => {
+//   test(`getTableData returns all events`, async done => {
+//     await refresh();
+//     expect(getAllData.getTableData("teams")).resolves.toEqual(teams);
+//     done();
+//   }, 30000);
+// });
+describe("getAllData function", () => {
+  test(`getTableData returns all events`, async () => {
     await refresh();
-    expect(getAllData.getTableData("events")).resolves.toEqual(events);
-    done();
-  }, 30000);
-});
-
-describe("getTableData returns all teams", () => {
-  test(`getTableData returns all events`, async done => {
-    await refresh();
-    expect(getAllData.getTableData("teams")).resolves.toEqual(teams);
-    done();
-  }, 30000);
+    await expect(getAllData.getTableData("events")).resolves.toEqual(events);
+    await expect(getAllData.getTableData("teams")).resolves.toEqual(teams);
+    await expect(getAllData.getTableData("tea")).rejects.toThrow(
+      "does not exist"
+    );
+  });
 });
