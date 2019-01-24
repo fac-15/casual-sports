0dd18abd843e144e82d2edd81df585de98b600bf
Sandra <grahlsan@gmail.com>
2019-01-24 14:19:51 +0000

    getAllData tests

--- src/queries/_tests_/getAllData.test.js
+++ src/queries/_tests_/getAllData.test.js
@@ -2,34 +2,19 @@ const getAllData = require("../getAllData");
 const refresh = require("../../db/build.js");
 const { events, teams } = require("./test_fixtures");
 
-test("the function returns all of the event data", () => {
-  refresh.refresh((error, response) => {
-    if (error) return console.log("ERROR IN DBBUILD: " + error);
-    expect.assertions(1);
-    return expect(getAllData.getTableData("events")).resolves.toEqual(events);
-  });
-});
+test("the function returns all of the event data", () => 
+expect(getAllData.getTableData("events")).resolves.toEqual(events)
+);
 
-test("the function fails with an error", () => {
-  refresh.refresh((error, response) => {
-    if (error) return console.log("ERROR IN DBBUILD: " + error);
-    const msg = "OMG EXPLOSIONS";
-    return expect(getAllData.getTableData(new Error())).rejects.toThrow(msg);
-  });
-});
+test("the function returns all of the team data", () => 
+expect(getAllData.getTableData("teams")).resolves.toEqual(teams)
+);
 
-test("the function returns all of the team data", () => {
-  refresh.refresh((error, response) => {
-    if (error) return console.log("ERROR IN DBBUILD: " + error);
-    expect.assertions(1);
-    return expect(getAllData.getTableData("teams")).resolves.toEqual(teams);
-  });
-});
+test("the function fails with an error", () => 
+   expect(getAllData.getTableData("blfojffoifjoffoif")).toBe.rejected
+  );
 
-test("the function fails with an error", () => {
-  refresh.refresh((error, response) => {
-    if (error) return console.log("ERROR IN DBBUILD: " + error);
-    const msg = "OMG EXPLOSIONS";
-    return expect(getAllData.getTableData(new Error())).rejects.toThrow(msg);
-  });
-});
+
+  test("the function fails with an error", () => 
+  expect(getAllData.searchSport("blfojffoifjoffoif")).toBe.rejected
+ );
