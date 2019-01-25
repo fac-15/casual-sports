7b23cf3c330ba1a61ed07a9841bb75f31be2d8ad
saksiva <sakatheesan@gmail.com>
2019-01-25 09:22:06 +0000

    add to get data searchSport test

--- src/queries/_tests_/getAllData.test.js
+++ src/queries/_tests_/getAllData.test.js
@@ -1,6 +1,6 @@
 const getAllData = require("../getAllData");
 const { refresh } = require("../../db/build.js");
-const { events, teams } = require("./test_fixtures");
+const { events, teams, football, cricket } = require("./test_fixtures");
 
 // describe("testing getTableData", () => {
 //   test(`getTableData returns all events`, async done => {
@@ -16,13 +16,28 @@ const { events, teams } = require("./test_fixtures");
 //     expect(getAllData.getTableData("teams")).resolves.toEqual(teams);
 //     done();
 //   }, 30000);
-// });
+//
 describe("getAllData function", () => {
   test(`getTableData returns all events`, async () => {
     await refresh();
     await expect(getAllData.getTableData("events")).resolves.toEqual(events);
     await expect(getAllData.getTableData("teams")).resolves.toEqual(teams);
     await expect(getAllData.getTableData("tea")).rejects.toThrow(
+      "does not exist"
+    );
+  });
+});
+
+describe("searchSport function", () => {
+  test(`searchSport returns correct results`, async () => {
+    await refresh();
+    await expect(getAllData.searchSport("events", "football")).resolves.toEqual(
+      football
+    );
+    // await expect(getAllData.searchSport("events", "cricket")).reject.toThrow(
+    //   err
+    // );
+    await expect(getAllData.searchSport("tea", "football")).rejects.toThrow(
       "does not exist"
     );
   });
